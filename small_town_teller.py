634263e837b6edeb747046cfcbce7402c787da49
Jason <jason@Zipcoders-MacBook-Pro.local>
2022-03-20 15:54:39 -0400

    bank

--- small_town_teller.py
+++ small_town_teller.py
@@ -4,10 +4,18 @@ class Person:
         self.first_name = first_name
         self.last_name = last_name
 
-class Acount:
+class Account:
     def __init__(self, number, type, owner, balance):
         self.number = number
         self.type = type
         self.owner = owner
         self.balance = balance
+
+class Bank:
+    def __init__(self):
+        self.person
+        self.account
+
+    def add_customer(self, first_name, last_name):
+        id
 
