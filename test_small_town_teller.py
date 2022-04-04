dc556a98cb74646fec18c8253932ebd4a24164c6
Jason <jason@Zipcoders-MacBook-Pro.local>
2022-04-04 15:30:55 -0400

    update

--- test_small_town_teller.py
+++ test_small_town_teller.py
@@ -10,5 +10,29 @@ class SmallTownTellerTest(unittest.TestCase):
         self._jane_savings = small_town_teller.Account(456, 'SAVINGS', self._jane)
         self._walter_checking = small_town_teller.Account(123, 'CHECKING', self._walter)
         self._bank = small_town_teller.Person()
+
+    def test_person_initialization(self):
+        expected_id = 1
+        expected_first_name = 'John'
+        expected_las_name = 'Smith'
+
+        self.assertEqual(expected_id, self._john.id)
+        self.assertEqual(expected_first_name, self._john.first_name)
+        self.assertEqual(expected_las_name, self._john.last_name)
+
+    def test_account_initialization(self):
+        expected_number = 123
+        expected_type = 'CHECKING'
+        expected_owner = 'id: 1. owner: John Smith'
+        expected_balance = 0
+
+        self.assertEqual(expected_number, self._john_checking.number)
+        self.assertEqual(expected_type, self._john_checking.type)
+        self.assertEqual(expected_owner, str(self._john_checking.owner))
+        self.assertEqual(expected_balance, self._john_checking.balance)
+
+
+
+
 if __name__ == '__main__':
     unittest.main()
